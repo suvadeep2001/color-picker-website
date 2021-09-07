4169987c3d635711f173b40e9170d443be0fed25
Suvadeep Maity <68159874+suvadeep2001@users.noreply.github.com>
2021-09-08 01:16:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./App.css";
+import { SwatchesPicker } from "react-color";
+
+function App() {
+  const [color, setColor] = useState("#6AC47E");
+  const [hidden, setHidden] = useState(false);
+  const pickerStyles = {
+    default: {
+      picker: {
+        position: "absolute",
+        bottom: "30px",
+        left: "100px",
+      },
+    },
+  };
+  return (
+    <div style={{ background: color }} className="App">
+      <div className="container">
+        {hidden && (
+          <SwatchesPicker
+            styles={pickerStyles}
+            color={color}
+            onChange={(updatedColor) => setColor(updatedColor.hex)}
+          />
+        )}
+
+        <button onClick={() => setHidden(!hidden)}>
+          {hidden ? "Close Color picker" : "Open Color picker"}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
